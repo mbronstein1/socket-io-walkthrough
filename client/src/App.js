c8e46c7f6a17cfddbfca86ad31277fa2eb07519f
Matthew Bronstein <mbronstein1@gmail.com>
2023-02-06 16:16:37 -0600

    added socket.io functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
-  return <div className='App'></div>;
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
+
+  const sendMessage = () => {
+    socket.emit('send_message', { message });
+  };
+
+  const onChangeHandler = e => {
+    setMessage(e.target.value);
+  };
+
+  useEffect(() => {
+    socket.on('receive_message', data => {
+      setMessageReceived(data.message);
+    });
+  }, []);
+
+  return (
+    <div className='App'>
+      <input
+        placeholder='Message...'
+        onChange={onChangeHandler}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <h1>Message: </h1>
+      {messageReceived}
+    </div>
+  );
 }
 
 export default App;
